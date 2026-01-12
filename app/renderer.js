f6820da42ca3590ca6e1443fcadf44d724ff5ef5
ASETML <alban.segalen@eduvaud.ch>
2026-01-12 13:59:30 +0100

    Preview en live

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,29 @@
-document.getElementById('convertButton').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    showPreview();
+}, false);
+
+document.getElementById('editor').addEventListener('keydown', function(e) {
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) +
+      "\t" + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;
+  }
+});
+
+const showPreview = () => {
     const md = document.getElementById("editor").value
     const html = markdown.parse(md)
     console.log(html);
     document.getElementById("preview").innerHTML = html
-});
+}
+
+var area = document.getElementById("editor");
+area.addEventListener('input', showPreview, false);
